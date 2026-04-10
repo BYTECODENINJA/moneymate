0fb322370d93d23c232d0355cc0d319d26fd17e9
Joseph <josephmulwa808@outlook.com>
2026-04-10 16:59:13 +0300

    start from here

---
+++ backend/src/db/index.ts
@@ -0,0 +1,11 @@
+import * as mongoose from "mongoose";
+
+const connectDb = async () => {
+    try {
+       const connectionInstance = await mongoose.connect(process.env.MONGODB_URI!);
+        console.log('Connected to MongoDB', connectionInstance.connection.host);
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+        process.exit(1);
+    }
+};
